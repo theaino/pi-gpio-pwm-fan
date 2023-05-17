30e9fe4e70a175b9840953cbed7986dc00097443
Aino Spring <info@aino-spring.com>
2023-05-17 20:49:29 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+import RPi.GPIO as GPIO
+import json
+import os
+import time
+
+
+def load_conf(path):
+    with open(path, "r") as conf_file:
+        return json.load(conf_file)
+
+
+def calc_speed(temp, min_temp, max_temp, curve):
+    temp_range = max_temp - min_temp
+    temp_delta = (temp - min_temp) / temp_range
+    return max(min(curve(temp_delta), 1), 0)
+
+
+def fetch_temp(cmd):
+    temp = os.popen(cmd).readline()
+    return int(temp)
+
+
+def main():
+    conf = load_conf("config.json")
+    fan = GPIO.PWM(conf.get("fan_pin"))
+
+    def curve(td):
+        return eval(conf.get("curve"))
+
+    try:
+        while True:
+            temp = fetch_temp(conf.get("cmd"))
+            speed = calc_speed(temp, conf.get("min_temp"), conf.get("max_temp"), curve)
+            fan.start(speed * 100)
+            time.sleep(conf.get("refresh_time") / 1000)
+    except KeyboardInterrupt:
+        print("Program exited by user.")
+
+
+if __name__ == "__main__":
+    if not os.path.isfile("config.json"):
+        with open("default_config.json", "r") as def_conf_file:
+            with open("config.json", "w") as conf_file:
+                conf_file.write(def_conf_file.read())
+    main()
