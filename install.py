c724b5d66de957d0efcf4ab8abcef477dde08e13
Aino Spring <info@aino-spring.com>
2023-05-18 13:42:24 +0200

    new install script

--- install.py
+++ install.py
@@ -6,10 +6,14 @@ import sys
 def install():
     path = pathlib.Path(os.path.abspath(__file__)).parent
     cmd = f"{sys.executable} {os.path.join(path, 'main.py')}"
-    with open("/etc/rc.local", "r+") as f:
-        original_data = f.read()
-        f.write(original_data.replace("exit 0", f"{cmd} & exit 0"))
+    with open("/etc/systemd/pi-gpio-pwm-fan.conf", "w") as f:
+        f.write(f"""start on runlevel [2345]
+stop on runlevel [!2345]
+
+exec {cmd}""")
+        os.system("service pi-gpio-pwm-fan start")
 
 
 if __name__ == "__main__":
     install()
+    print("Finished installing.")
