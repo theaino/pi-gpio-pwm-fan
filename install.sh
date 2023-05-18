db06a9bd73de13907d51f8b56d012aa5ab27b725
Aino Spring <info@aino-spring.com>
2023-05-18 17:41:53 +0200

    bug fix

--- install.sh
+++ install.sh
@@ -5,4 +5,5 @@ chmod +x /usr/bin/pi-gpio-pwm-fan/main.py
 printf "[Unit]\nDescription=GPIO PWM fan temperature control.\n\n[Service]\nType=simple\nExecStart=python3 /usr/bin/pi-gpio-pwm-fan/main.py\n\n[Install]\nWantedBy=multi-user.target" > /lib/systemd/system/pi-gpio-pwm-fan.service
 sudo cp /lib/systemd/system/pi-gpio-pwm-fan.service /etc/systemd/system/pi-gpio-pwm-fan.service
 sudo chmod 644 /etc/systemd/system/pi-gpio-pwm-fan.service
+systemctl enable pi-gpio-pwm-fan
 systemctl start pi-gpio-pwm-fan
